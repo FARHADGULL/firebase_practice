3771bd26282545f777b006db052461b4008d69e8
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-03-26 04:37:41 +0500

    Update app_routes.dart

--- lib/helpers/app_routes.dart
+++ lib/helpers/app_routes.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_practice/screens/auth/signup/signup_view.dart';
 import 'package:get/get.dart';
 
-import '../screens/login/login_view.dart';
+import '../screens/home/home_view.dart';
+import '../screens/auth/login/login_view.dart';
 
 class AppRoutes {
   static const initRoute = '/';
@@ -14,6 +16,8 @@ class AppRoutes {
     return [
       GetPage(name: initRoute, page: () => LoginView()),
       GetPage(name: loginViewRoute, page: () => LoginView()),
+      GetPage(name: homeRoute, page: () => HomeView()),
+      GetPage(name: signUpViewRoute, page: () => SignUpView()),
     ];
   }
 }
