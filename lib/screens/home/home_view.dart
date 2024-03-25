8e6b3f919798e13a972b207c60a117aff6591189
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-03-26 04:38:07 +0500

    Create home_view.dart

---
+++ lib/screens/home/home_view.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_practice/screens/home/home_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeView extends StatelessWidget {
+  HomeView({super.key});
+
+  final HomeViewModel viewModel = Get.put(HomeViewModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Text('WELCOME!\n${viewModel.user.email}'),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: viewModel.signOut,
+                  child: const Text('Sign Out'),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
