ef9355a2b33d01527a39fcf33a4efdb75672f519
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-03-26 04:38:10 +0500

    Create home_viewmodel.dart

---
+++ lib/screens/home/home_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class HomeViewModel extends GetxController {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
