7a8e45b302fb6f30639716ec77f485b5e1daa1c0
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-03-26 04:38:05 +0500

    Create signup_viewmodel.dart

---
+++ lib/screens/auth/signup/signup_viewmodel.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_practice/helpers/global_variable.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignUpViewModel extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  GlobalKey<FormState> signUpFormKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> signUp() async {
+    GlobalVariable.showLoader.value = true;
+    try {
+      if (signUpFormKey.currentState?.validate() ?? true) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim(),
+        );
+      }
+
+      GlobalVariable.showLoader.value = false;
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+
+      GlobalVariable.showLoader.value = false;
+    }
+  }
+}
