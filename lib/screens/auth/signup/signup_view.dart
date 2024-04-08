9b7f093216fe4c462903139a1240235ab903d822
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-04-08 22:55:49 +0500

    Update signup_view.dart

--- lib/screens/auth/signup/signup_view.dart
+++ lib/screens/auth/signup/signup_view.dart
@@ -12,77 +12,75 @@ class SignUpView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Sign Up'),
-        // ),
-        body: Stack(
-      children: [
-        SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: signUpViewModel.emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: signUpViewModel.emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                    ),
+                    textInputAction: TextInputAction.next,
                   ),
-                  textInputAction: TextInputAction.next,
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: signUpViewModel.passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: signUpViewModel.passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                    ),
+                    textInputAction: TextInputAction.next,
+                    obscureText: true,
                   ),
-                  textInputAction: TextInputAction.next,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: signUpViewModel.confirmPasswordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Confirm Password',
-                    hintText: 'Re-enter your password',
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: signUpViewModel.confirmPasswordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: 'Re-enter your password',
+                    ),
+                    textInputAction: TextInputAction.done,
+                    obscureText: true,
                   ),
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: signUpViewModel.signUp,
-                    child: const Text('Sign Up'),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: signUpViewModel.signUp,
+                      child: const Text('Sign Up'),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                // RichText(
-                //   text: TextSpan(
-                //     text: 'Already have an account? ',
-                //     style: const TextStyle(color: Colors.black),
-                //     children: [
-                //       TextSpan(
-                //         text: 'Sign In',
-                //         style: const TextStyle(
-                //           color: Colors.blue,
-                //         ),
-                //         recognizer: TapGestureRecognizer()
-                //           ..onTap = () {
-                //             Navigator.of(context).pop();
-                //           },
-                //       ),
-                //     ],
-                //   ),
-                // ),
-              ],
+                  const SizedBox(height: 20),
+                  // RichText(
+                  //   text: TextSpan(
+                  //     text: 'Already have an account? ',
+                  //     style: const TextStyle(color: Colors.black),
+                  //     children: [
+                  //       TextSpan(
+                  //         text: 'Sign In',
+                  //         style: const TextStyle(
+                  //           color: Colors.blue,
+                  //         ),
+                  //         recognizer: TapGestureRecognizer()
+                  //           ..onTap = () {
+                  //             Navigator.of(context).pop();
+                  //           },
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
           ),
-        ),
-        const LoaderView(),
-      ],
-    ));
+          const LoaderView(),
+        ],
+      ),
+    );
   }
 }
