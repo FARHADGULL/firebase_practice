724153032d15bd9eef63e3894eea77b23ec967ba
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-04-08 23:31:26 +0500

    Update home_viewmodel.dart

--- lib/screens/home/home_viewmodel.dart
+++ lib/screens/home/home_viewmodel.dart
@@ -2,11 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class HomeViewModel extends GetxController {
-  final user = FirebaseAuth.instance.currentUser!;
+  RxString user = FirebaseAuth.instance.currentUser!.email!.obs;
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
-
-    user.delete();
   }
 }
